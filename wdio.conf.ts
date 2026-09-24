27d250f5ab0cc7d534e1296c36024e4c38455f85
Felipe <felipee.cssz@gmail.com>
2026-09-23 22:02:47 -0300

    fix: bloqueia anúncios de terceiros para estabilizar os testes

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -54,7 +54,7 @@ export const config: WebdriverIO.Config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-    capabilities: [
+  capabilities: [
     {
       browserName: 'chrome',
       'goog:chromeOptions': {

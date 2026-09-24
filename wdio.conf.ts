1b9c1ea8f8ac102dfe22e8f517f11b977e434d93
Felipe <felipee.cssz@gmail.com>
2026-09-23 21:33:01 -0300

    chore: preenche .env.example e formata wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -91,7 +91,7 @@ export const config: WebdriverIO.Config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
- baseUrl: process.env.BASE_URL,
+  baseUrl: process.env.BASE_URL,
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
